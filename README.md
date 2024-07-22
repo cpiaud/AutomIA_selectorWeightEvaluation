cdf2f0a0e552e29a33002b5c61de3a1453f25151
Abderraouf GUESMI <AGU24909@open-groupe.com>
2024-07-22 15:23:38 +0200

    add 2 approches

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # AutomIA_selectorWeightEvaluation
 Use Machine Learning to evaluate Weight of properties to find Element with AutomIA_ElementFinder.
+
 Ce travail consite à déterminer les poids de chaque propriété en utilisant les méthodes de machine learning(clustring) et de l'IA(OpenAI) :
 - Le fichier en sortie de machine learning doit s'appeller "selectorWeight.properties" et être constitué d'une liste de pair clé:valeur.
 - La Clé est un attribut/propriété d'un élément (ex: id, class, name, type...)
 - La valeur est un poids de 0 à 100 qui donne l'importance de l'attribut pour retrouver un élément par rapport aux autres attributs.
 
-## Clustring avec K-means
+## Approche 01: Clustring avec K-means
+
 K-means partitionne 𝑛 observations en 𝑘 clusters en assignant chaque point au centre de cluster le plus proche. Les centres de clusters sont recalculés comme la moyenne des points assignés, et le processus est répété jusqu'à convergence. L'algorithme minimise la variance intra-cluster pour une classification optimale.
 Pour utiliser cette méthode , on va suivre les étapes suivantes:
 ### 1- Préparation des données
 La préparation des données consiste à structurer nos données sous forme CSV et remplir les cases vides par "n"(no data) .
-
 Voir un exemple de préparation sous forme data.csv
-
 ### 2- Configuration des entrées/sorties
-Configuration des parametres dans un fichier config.txt sous la forme:
+Configuration des parametres dans un fichier config_kmeans.txt sous la forme:
 
 [base]
 
@@ -42,10 +42,9 @@ prop_columns =
 Voir un exemple de fichier : config.txt
 
 ## 3-Installation et exécution 
-- Créer et activer un environement virtuel :
+- Créer un environement virtuel :
 ```bash
 Python -m venv myenv
-venv\Scripts\activate
 ```
 
 - Créer un environement virtuel :
@@ -57,8 +56,58 @@ pip install -r requirements.txt
 ```bash
 python kmeans_clustring_approch1.py
 ```
-## Done!
-## ToDO
-- Proposer Autres approches de Machine Learning.
-- Utiliser Openai comme une solution.
-- Comparer les résultats et déployer la solution.
+
+## Approche 02: Calculer les poids avec OpenAI
+L'API d'OpenAI permet d'intégrer des capacités avancées d'intelligence artificielle dans les applications. Elle offre des fonctionnalités telles que la génération de texte, la compréhension du langage, et la création de réponses intelligentes, basées sur des modèles de traitement du langage naturel comme GPT-4. Les utilisateurs peuvent envoyer des requêtes et recevoir des réponses adaptées à des besoins variés, allant de la création de contenu à l'assistance client. Pour accéder à l'API, une clé d'API est nécessaire et l'utilisation est généralement facturée en fonction du volume de requêtes.
+Pour utiliser cette méthode , on va suivre les étapes suivantes:
+### 1-Géneration d'un token OpenAI
+- Créer un compte sur : https://platform.openai.com/
+- Créer un projet et configurez les options de facturation.
+- Générer la clé API et copiez-la dans un fichier sécurisé api_key_openai.txt sous configs.
+### 2- Préparation des données(meme que l'approche 1)
+La préparation des données consiste à structurer nos données sous forme CSV et remplir les cases vides par "n"(no data) .
+Voir un exemple de préparation sous forme data.csv
+### 3- Configuration des entrées/sorties
+Configuration des parametres dans un fichier config_gpt.txt sous la forme:
+
+[base]
+file_path=data.csv
+output_file=selectorWeight_data_gpt.properties
+
+[columns_csv]
+cols=ElementName, Langage, Tag, Prop1, Prop2, Prop3, Prop4, Prop5
+
+Voir un exemple de fichier : config_gpt.txt.
+Lister dans le code open_ai_approch2.py dans la section messages les propriétés:
+```bash
+        - data-focus
+        - label
+        - id
+        - profil-list
+        - aria-label
+        - class
+        - text
+        - name
+        - for
+        - grid
+        - index
+```
+
+## 4-Installation et exécution 
+- Créer un environement virtuel :
+```bash
+Python -m venv myenv
+```
+
+- Créer un environement virtuel :
+```bash
+pip install -r requirements.txt
+```
+
+- Exécuter le code:
+```bash
+python open_ai_approch2.py
+```
+## Resultats
+Les résultats obtenus avec les deux approches sont comparables en ce qui concerne la force des poids obtenus. 
+NB : les valeurs des poids ne sont pas identiques pour les deux approches, mais elles peuvent être expliquées de la même manière.
--- kmeans_clustring_approch1.py
+++ kmeans_clustring_approch1.py
@@ -1,81 +1,119 @@
+#import packages
 import pandas as pd
 from sklearn.cluster import KMeans
 from collections import Counter, OrderedDict
 import configparser
 import os
-def load_config(file_path):
-   
-    config = configparser.ConfigParser()
-    config.read(file_path)
-    
-    file_path = config.get('base', 'file_path')
-    output_file = config.get('base', 'output_file')
-    num_clusters = config.getint('base', 'num_clusters')
-    properties_str = config['properties_dict']
-    print(properties_str.items())
-    properties_dict = {}
-    for key, value in properties_str.items():
-        cleaned_key = key.strip().strip("'")
-        cleaned_value = int(value.strip().rstrip(','))
-        properties_dict[cleaned_key] = cleaned_value
+from utils import load_config,load_data
+#current folder
+script_dir = os.path.dirname(os.path.abspath(__file__))
+def preprocess_properties(data, properties_dict, prop_columns):
+    """ 
+    Transforme les propriétés dans un DataFrame en utilisant le dictionnaire de propriétés fourni.
 
-    prop_columns = config.get('prop_columns', 'prop_columns').split(', ')
-    columns_csv=config.get('columns_csv', 'cols').split(', ')
-    return file_path,columns_csv, properties_dict, prop_columns, num_clusters, output_file
+    Arguments:
+    data -- DataFrame contenant les données à transformer. 
+    properties_dict -- Dictionnaire où les clés sont les valeurs actuelles des propriétés et les valeurs sont les nouvelles valeurs désirées.
+    prop_columns -- Liste des noms des colonnes dans le DataFrame 'data' qui contiennent les propriétés à transformer.
 
-
-def load_data(file_path,usecols):
-    try:
-        data = pd.read_csv(file_path, sep=";", usecols=usecols)
-        return data
-    except FileNotFoundError:
-        print(f"Erreur: Le fichier {file_path} n'a pas été trouvé.")
-        return None
-
-def preprocess_properties(data, properties_dict,prop_columns):
-    """ Transforme les propriétés en utilisant le dictionnaire de propriétés fourni. """
+    Retourne:
+    DataFrame -- Le DataFrame 'data' avec les propriétés transformées selon 'properties_dict'.
+    """
     for prop in prop_columns:
         data[prop] = data[prop].map(properties_dict)
     return data
 
 def apply_kmeans(data, prop_columns, num_clusters):
-    #Applique l'algorithme K-means 
+    """
+    Applique l'algorithme K-means sur des colonnes spécifiques du DataFrame et calcule les poids des clusters.
+
+    Arguments:
+    data -- DataFrame contenant les données à analyser. Les colonnes spécifiées doivent être numériques.
+    prop_columns -- Liste des noms des colonnes sur lesquelles appliquer K-means.
+    num_clusters -- Nombre de clusters à créer avec l'algorithme K-means.
+
+    Retourne:
+    list_dict_weights -- Liste de dictionnaires, où chaque dictionnaire contient les poids des clusters 
+                         pour une colonne spécifique.
+    """
+    # Initialise les centres de clusters comme une liste de listes contenant des indices allant de 0 à num_clusters-1.
     initial_centers = [[i] for i in range(num_clusters)]
+    
+    # Liste pour stocker les poids des clusters pour chaque colonne traitée.
     list_dict_weights = []
 
+    # Itère à travers chaque colonne spécifiée pour appliquer K-means.
     for prop in prop_columns:
+        # Création et configuration du modèle KMeans.
         kmeans = KMeans(n_clusters=num_clusters, random_state=0, n_init=1, init=initial_centers)
+        
+        # Application de K-means sur la colonne actuelle et ajout des labels de clusters au DataFrame.
         data[f'cluster_{prop}'] = kmeans.fit_predict(data[[prop]])
         
+        # Comptage du nombre d'échantillons dans chaque cluster.
         cluster_counts = Counter(data[f'cluster_{prop}'])
-        total = sum(cluster_counts.values())
-        weights = {k: round(v / total * 100) for k, v in cluster_counts.items()}
+        
+        # Calcul des poids des clusters en pourcentage.
+        total = sum(cluster_counts.values())  # Nombre total d'échantillons
+        weights = {k: round(v / total * 100) for k, v in cluster_counts.items()}  # Poids en pourcentage
+        
+        # Ajout des poids des clusters pour cette colonne à la liste des poids.
         list_dict_weights.append(weights)
     
+    # Retourne la liste des dictionnaires de poids des clusters pour chaque colonne traitée.
     return list_dict_weights
 
+
 def combine_weights(list_weights):
-    #Combine et trie les poids de tous les clusters.
+    """
+    Combine et trie les poids des clusters de plusieurs dictionnaires en un seul dictionnaire trié.
+
+    Arguments:
+    list_weights -- Liste de dictionnaires où chaque dictionnaire contient les poids des clusters pour une colonne donnée.
+
+    Retourne:
+    sorted_weights -- Dictionnaire ordonné avec les poids combinés et triés par clé.
+    """
+    # Combine les poids de tous les dictionnaires dans list_weights en un seul dictionnaire.
+    # Utilise Counter pour additionner les valeurs associées aux mêmes clés dans chaque dictionnaire.
     combined_weights = sum((Counter(d) for d in list_weights), Counter())
+    
+    # Trie les poids combinés par clé et crée un OrderedDict pour maintenir l'ordre.
     sorted_weights = OrderedDict(sorted(combined_weights.items()))
+    
+    # Retourne le dictionnaire trié avec les poids combinés.
     return sorted_weights
 
-def write_to_properties_file(final_dict,properties_dict , file_path):
-    #Écrit le dictionnaire final dans un fichier de propriétés selon le format spécifié.
-    final_dict_poids = {key: final_dict[value] for key, value in properties_dict .items()}
+
+def write_to_properties_file(final_dict, properties_dict, file_path):
+    """
+    Écrit le dictionnaire final dans un fichier de propriétés au format clé=valeur.
+
+    Arguments:
+    final_dict -- Dictionnaire contenant les poids finaux à écrire dans le fichier.
+    properties_dict -- Dictionnaire qui mappe les clés du fichier de propriétés aux clés du dictionnaire final.
+    file_path -- Chemin du fichier où les propriétés doivent être écrites.
+
+    Aucun retour.
+    """
+    # Prépare un dictionnaire pour le fichier de propriétés en mappant les clés de properties_dict 
+    # aux valeurs correspondantes dans final_dict.
+    final_dict_poids = {key: final_dict[value] for key, value in properties_dict.items()}
+    
+    # Supprime l'entrée avec la clé 'n' si elle existe dans le dictionnaire préparé.
     del final_dict_poids['n']
+    
+    # Ouvre le fichier à l'emplacement spécifié en mode écriture.
     with open(file_path, 'w') as file:
+        # Écrit chaque clé et valeur dans le fichier sous la forme 'clé=valeur'.
         for key, value in final_dict_poids.items():
             file.write(f"{key}={value}\n")
 
+
 def main():
-    config_file = 'config.txt'
-    if not os.path.exists(config_file):
-        print(f"Fichier de configuration {config_file} introuvable.")
-        return
-    
+
     # Charger la configuration à partir du fichier config.txt
-    file_path,columns_csv, properties_dict, prop_columns, num_clusters, output_file = load_config(config_file)
+    file_path,columns_csv, properties_dict, prop_columns, num_clusters, output_file = load_config(script_dir,'config_kmeans.txt')
     data = load_data(file_path, usecols=columns_csv)
     if data is None:
         return
@@ -90,7 +128,7 @@ def main():
     final_weights = combine_weights(list_weights)
     
     # Écriture dans le fichier de propriétés
-    write_to_properties_file(final_weights,properties_dict, output_file)
+    write_to_properties_file(final_weights,properties_dict, os.path.join(script_dir+'/weights', output_file))
     
     # Affichage des résultats
     print(data)
